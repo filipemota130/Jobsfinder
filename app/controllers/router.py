9cfcfccd1ab8ad7cfcd00e46304dd06d01396800
filipemota12 <filipe_simoes@outlook.com>
2021-03-05 22:09:07 -0300

    feature: cadastro de usuário funcionando!

--- app/controllers/router.py
+++ app/controllers/router.py
@@ -1,5 +1,6 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 from app import app, db
+from werkzeug.security import generate_password_hash
 
 from app.models.user import User
 from app.models.forms import LoginForm, CadastroForm
@@ -14,37 +15,43 @@ def index(id):
 @app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
     form = CadastroForm()
-    if form.validate_on_submit():
-        print(form.email.data)
-        print(form.password.data)
-        print(form.name.data)
-        print(form.nick_name.data)
-        print(form.contact.data)
-        print(form.birth_date.data)
-        # i = User(form.name.data, form.nick_name.data,
-        #        form.password.data, form.email.data, form.contact.data, form.birth_date.data, form.desc.data)
-    else:
-        print(form.email.errors)
-        print(form.password.errors)
-        print(form.name.errors)
-        print(form.nick_name.errors)
-        print(form.contact.errors)
-        print(form.birth_date.errors)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            hashed_password = generate_password_hash(form.password.data, method = 'sha256')
+            password = hashed_password
+            new_user = User(form.name.data, form.nick_name.data,
+                    password, form.email.data, form.contact.data, form.birth_date.data, form.desc.data)
+            confirmation = User.query.filter_by(email= form.email.data).first()
+            if confirmation == None :
+                db.session.add(new_user)
+                db.session.commit()
+                return redirect(url_for('index',id = new_user.id))
+            else : 
+                flash("usuário já existente")
+        else:
+            print(form.email.errors)
+            print(form.password.errors)
+            print(form.name.errors)
+            print(form.nick_name.errors)
+            print(form.contact.errors)
+            print(form.birth_date.errors)
+
     return render_template("cadastro.html", form=form)
 
 
-@app.route('/login', methods=['GET', 'POST'], defaults={'info': None})
-def login(info):
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     form = LoginForm()
     if request.method == 'POST':
         if form.validate_on_submit():
-            r = User.query.filter_by(email= form.email.data, password =form.password.data).first()
-            if r:
-                db.session.add(r)
+            flash("Login confirmado!")
+            instance = User.query.filter_by(email= form.email.data, password =form.password.data).first()
+            if instance:
+                db.session.add(instance)
                 db.session.commit()
-                return redirect(url_for('index',id = r.id))
+                return redirect(url_for('index',id = instance.id))
             else :
-                return("usuário inexistente")
+                flash("usuário inexistente")
         else:
             print(form.email.errors)
             print(form.password.errors)
@@ -60,3 +67,10 @@ def perfil():
 @app.route('/jobs')
 def jobs():
     return 'Jobs page'
+
+
+@app.route('/logout')
+def logout():
+    flash("Sessão encerrada!")
+    db.session.expire()
+    return redirect(url_for('index'))
